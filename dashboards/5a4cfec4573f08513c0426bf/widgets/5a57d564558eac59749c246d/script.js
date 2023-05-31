bb0da36a68c4f1ec7c570a592fc8415fbbd9351f
stephen.poff <stephen.poff%40sisense.com>
2023-05-31 16:12:37 +0000

    Test

---
+++ dashboards/5a4cfec4573f08513c0426bf/widgets/5a57d564558eac59749c246d/script.js
@@ -0,0 +1,13 @@
+widget.on('render',function(w,e){
+
+e.prefixText = '';
+e.widgetIds = [
+{id:'5a4d04ad573f08513c042948', panelToChange:'values', index:0},
+	{id:'5a4d0355573f08513c0428da', panelToChange:'values', index:0},
+	// for pivot insert 'rows', for charts insert 'values' 
+];
+
+});
+
+widget.on('render',function(w, e){e.selectedColor = '#004d4d';});
+
